30622b411f67a50ad6e43698d5e9ac2b73fb654c
deva-17 <dpdeva017@gmail.com>
2025-07-18 17:41:09 +0530

    initial commit - MERN damscraper

---
+++ backend/models/dam.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const damSchema = new mongoose.Schema({
+    damName: String,
+    fullDepth: Number,
+    fullCapacity: Number,
+    currentWaterLevel: Number,
+    currentStorageVolume: Number,
+    inflow: Number,
+    outflow: Number,
+    date: String,
+
+    // 👇 Add this line to link dam with a specific user
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+        required: true
+    }
+});
+
+module.exports = mongoose.model("Dam", damSchema);
