c1b47fc0e508e7117c39fbaaf7da1eec22592ce3
deva-17 <dpdeva017@gmail.com>
2025-07-26 11:26:13 +0530

    Initial commit

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ const userRoutes = require('./router/users')
 const authRoutes = require('./router/auth')
 const damRoutes = require('./router/damRouter');
 
+
 connection();
 
 app.use(express.json())
@@ -16,6 +17,7 @@ app.use(cors())
 app.use("/api/users",userRoutes)
 app.use("/api/auth",authRoutes)
 app.use('/ourdams',damRoutes);
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, ()=>console.log(`Listening on port ${port}...`))
